1cd50386a1fac02e2dd668759e87af08a023ef08
lzj1121 <42904862+lzj1121@users.noreply.github.com>
2022-10-14 16:07:04 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -11,4 +11,19 @@
 #
 
 # Modify default IP
-#sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+rm -rf feeds/luci/applications/luci-app-argon-config
+rm -rf feeds/luci/applications/luci-app-openclash
+rm -rf feeds/luci/applications/luci-app-vssr
+rm -rf feeds/luci/applications/luci-app-ssr-plus
+rm -rf feeds/luci/applications/luci-app-passwall
+rm -rf feeds/luci/themes/luci-theme-argon
+rm -rf feeds/packages/net/v2ray-core
+rm -rf feeds/packages/net/v2ray-plugin
+rm -rf feeds/packages/net/xray-core
+rm -rf feeds/packages/net/xray-plugin
+sed -i 's/192.168.1.1/10.10.10.1/g' package/base-files/files/bin/config_generate
+git clone https://github.com/jerrykuku/luci-theme-argon package/luci-theme-argon
+git clone https://github.com/jerrykuku/luci-app-argon-config package/luci-app-argon-config
+git clone https://github.com/messense/aliyundrive-webdav package/aliyundrive-webdav
+git clone -b luci https://github.com/xiaorouji/openwrt-passwall package/psw
+git clone -b packages https://github.com/xiaorouji/openwrt-passwall package/pswcore
